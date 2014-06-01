e58b69baa00932deb1a02ee5789fa54a3dfc1587
Alan Bishop <djab1990@gmail.com>
2014-06-01 13:39:58 -0700

    more work done on basic CRUD app blow

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
+include PostsHelper
+
 class PostsController < ApplicationController
+
     def index
         @posts = Post.all
     end
@@ -6,4 +9,39 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end 
+
+    def new
+        @post = Post.new
+    end 
+
+    def create
+        @post = Post.new
+        @post = Post.new(post_params)
+        @post.save
+        flash.notice = "Post '#{@post.title}' Created!"
+        redirect_to post_path(@post)
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        @post.update(post_params)
+        
+        flash.notice = "Post '#{@post.title}' Updated!"
+    
+        redirect_to post_path(@post)
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        flash.notice = "Post '#{@post.title}' Deleted!"
+        
+        redirect_to action: "index"
+    end 
+
 end
