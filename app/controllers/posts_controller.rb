7e251a62803e618e133b9dd96d1768ecf54bc126
Alan Bishop <djab1990@gmail.com>
2014-06-03 19:38:44 -0700

    Working on this blog some more, fixed some errors, still got more :(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,14 @@ class PostsController < ApplicationController
 
     def show
         @post = Post.find(params[:id])
+        @comment = Comment.new 
+        @comment.post_id = @post_id
     end 
 
     def new
         @post = Post.new
+        @comment = Comment.new 
+        @comment.post_id = @post_id
     end 
 
     def create
